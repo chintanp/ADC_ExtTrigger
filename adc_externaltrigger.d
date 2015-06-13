4f7ea4135bf0c35f10a6fe2a1ef33a77eadf10f6
Chintan Pathak <chintan.pathak@gmail.com>
2015-06-13 12:43:47 +0530

    First Commit

---
+++ adc_externaltrigger.d
@@ -0,0 +1,2 @@
+.\adc_externaltrigger.o: ADC_ExternalTrigger.c
+.\adc_externaltrigger.o: tm4c123gh6pm.h
---
+++ adc_mainuart.d
@@ -0,0 +1,5 @@
+.\adc_mainuart.o: ADC_MainUART.c
+.\adc_mainuart.o: ADCSWTrigger.h
+.\adc_mainuart.o: tm4c123gh6pm.h
+.\adc_mainuart.o: PLL.h
+.\adc_mainuart.o: UART.h
